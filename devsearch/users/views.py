f05d03fd14bd9d408520120525915c851ebdb61d
SAJ33L <sajeel.khan01@gmail.com>
2024-02-10 00:37:39 +0500

    added custom user form

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+
 from django.contrib.auth.models import User
 from .models import Profile
+from .forms import CustomUerCreationForm
 
 def loginUser(request):
+    page = 'login'
 
     if request.user.is_authenticated:
         return redirect('profiles')
@@ -33,6 +36,27 @@ def logoutUser(request):
     logout(request)
     messages.error(request, "User was logged out.")
     return redirect('login')
+
+def registerUser(request):
+    page = 'register'
+    form = CustomUerCreationForm()
+
+    if request.method == 'POST':
+        form = CustomUerCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, 'User Created Successfully!')
+            login(request, user)
+            return redirect('profiles')
+    else:
+        messages.error(request, 'An error occured during regisration!')
+
+    context = {'page': page,
+               'form': form}
+    return render(request, 'users/login_register.html', context)
 
 def profiles(request):
     profiles = Profile.objects.all()
