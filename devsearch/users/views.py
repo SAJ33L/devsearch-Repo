8bf97b18c1ee7706834badaa518e241a722faf81
SAJ33L <sajeel.khan01@gmail.com>
2024-02-09 23:34:38 +0500

    django flash messages added

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
 
@@ -15,7 +17,7 @@ def loginUser(request):
         try:
             user = User.objects.get(username=username)
         except:
-            print("Username does not exist.")
+            messages.error(request, "Username or Password is not correct.")
 
         user = authenticate(request, username=username, password=password)
 
@@ -29,7 +31,8 @@ def loginUser(request):
 
 def logoutUser(request):
     logout(request)
-    return redirect('logout')
+    messages.error(request, "User was logged out.")
+    return redirect('login')
 
 def profiles(request):
     profiles = Profile.objects.all()
